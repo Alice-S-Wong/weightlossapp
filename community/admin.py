eb3484efead294d12a4fc7f19972b5c3ca38fdee
Alice <alicexenawong@gmail.com>
2020-03-05 00:30:34 -0800

    Add admin editing capability

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question
+from .models import Answer
+from .models import BookmarkQuestion
+from .models import BookmarkAnswer
+
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(BookmarkQuestion)
+admin.site.register(BookmarkAnswer)
